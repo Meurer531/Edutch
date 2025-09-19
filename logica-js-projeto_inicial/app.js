772c083dcda7ce07cac87b96e45ad449e5b96091
Meurer531 <148451701+Meurer531@users.noreply.github.com>
2025-09-19 09:32:15 -0300

    .

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,11 +1,12 @@
-alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = parseInt(Math.random() * 100 + 1);
+alert('Seja Bem Vindo ao jogo do número secreto');
+let numeroMaximo = 5000;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
 console.log(numeroSecreto);
 let chute;
 let tentativas = 1;
 
 while (chute != numeroSecreto) {
-    chute = prompt('Escolha um número entre 1 e 100');
+    chute = prompt(`Escolha um número de 1 e ${numeroMaximo}`);
     if (chute == numeroSecreto) {
         break;
     } else {
@@ -19,4 +20,4 @@ while (chute != numeroSecreto) {
 }
 
 let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
-a
+alert(`Parabéns! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
